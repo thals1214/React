a900255a0dea2d8ec0da598b1f528ffa959d3f78
thals1214 <thaxls@ewhain.net>
2022-02-16 22:50:13 +0900

    react-router-tutorial

--- src/App.js
+++ src/App.js
@@ -1,97 +1,45 @@
 import React, { useState, useRef, useCallback } from 'react';
-import produce from 'immer';
+import { Route, Link, Switch } from 'react-router-dom';
+import About from './Router/About';
+import Home from './Router/Home';
+import Profile from './Router/Profile';
+import Profiles from './Router/Profiles';
+import HistorySample from './Router/HistorySample';
+import WithRouterSample from './Router/WithRouterSample';
 
 const App = () => {
-    const nextId = useRef(1);
-    const [form, setForm] = useState({ name: '', username: '' });
-    const [data, setData] = useState({
-        array: [],
-        uselessValue: null,
-    });
-
-    //input 수정을 위한 함수
-    const onChange = useCallback(
-        (e) => {
-            const { name, value } = e.target;
-            setForm(
-                produce((draft) => {
-                    draft[name] = value;
-                })
-            );
-        },
-        [form]
-    );
-
-    //form 등록을 위한 함수
-    const onSubmit = useCallback(
-        (e) => {
-            e.preventDefault(); //what is this
-            const info = {
-                id: nextId.current,
-                name: form.name,
-                username: form.username,
-            };
-
-            //array에 새 항목 등록
-            setData(
-                produce((draft) => {
-                    draft.array.push(info);
-                })
-            );
-
-            //form 초기화
-            setForm({
-                name: '',
-                username: '',
-            });
-            nextId.current += 1;
-        },
-        [data, form.name, form.username]
-    );
-
-    //항목을 삭제하는 함수
-    const onRemove = useCallback(
-        (id) => {
-            setData(
-                produce((draft) => {
-                    draft.array.splice(
-                        draft.array.findIndex((info) => info.id === id),
-                        1
-                    );
-                })
-            );
-        },
-        [data]
-    );
-
-    return (
-        <>
-            <form onSubmit={onSubmit}>
-                <input
-                    name="username"
-                    placeholder="아이디"
-                    value={form.username}
-                    onChange={onChange}
-                />
-                <input
-                    name="name"
-                    placeholder="이름"
-                    value={form.name}
-                    onChange={onChange}
-                />
-                <button type="submit">등록</button> {/*버튼에도 타입 존재? */}
-                <>
-                    <ul>
-                        {data.array.map((info) => (
-                            <li key={info.id} onClick={() => onRemove(info.id)}>
-                                {info.username} ({info.name})
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            </form>
-        </>
-    );
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
